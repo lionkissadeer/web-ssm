1610d4f4b35141543a87626157bd5cffccbfd74b
lionkissadeer <15951723026@163.com>
2017-04-20 20:27:35 +0800

    提交

--- src/main/java/com/skytech/ssm/dao/IUserDao.java
+++ src/main/java/com/skytech/ssm/dao/IUserDao.java
@@ -2,10 +2,13 @@ package com.skytech.ssm.dao;
 
 
 import com.skytech.ssm.entity.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IUserDao {
 
-	List<User> queryForUser();
+    List<User> queryForUser();
+    List<User> selectForUser();
 }
